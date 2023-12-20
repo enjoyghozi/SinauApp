8c6891c1e6a90ff26a8b223d14d83c3aa5e2c987
enjoyghozi <adamghosi03@gmail.com>
2023-12-20 17:11:07 +0700

    feat(ui) : MapelCardsSection

---
+++ app/src/main/java/com/example/sinauapp/domain/model/Mapel.kt
@@ -0,0 +1,18 @@
+package com.example.sinauapp.domain.model
+
+import androidx.compose.ui.graphics.Color
+import com.example.sinauapp.ui.theme.gradient1
+import com.example.sinauapp.ui.theme.gradient2
+import com.example.sinauapp.ui.theme.gradient3
+import com.example.sinauapp.ui.theme.gradient4
+import com.example.sinauapp.ui.theme.gradient5
+
+data class Mapel(
+    val name: String,
+    val totalJamBelajar: Float,
+    val colors: List<Color>,
+) {
+    companion object {
+        val mapelCardColor = listOf(gradient1, gradient2, gradient3, gradient4, gradient5)
+    }
+}
--- app/src/main/java/com/example/sinauapp/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/sinauapp/ui/home/HomeScreen.kt
@@ -1,19 +1,34 @@
 package com.example.sinauapp.ui.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.example.sinauapp.R
+import com.example.sinauapp.domain.model.Mapel
 import com.example.sinauapp.ui.components.CountCard
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -36,7 +51,12 @@ fun HomeScreen() {
                     jamBelajar = "10",
                     totalJamBelajar = "20"
                 )
-
+            }
+            item {
+                MapelCardsSection(
+                    modifier = Modifier.fillMaxWidth(),
+                    mapelList = emptyList()
+                )
             }
         }
     }
@@ -81,5 +101,47 @@ private fun CountCardSection(
             count = totalJamBelajar
         )
     }
-    
+}
+
+@Composable
+private fun MapelCardsSection(
+    modifier: Modifier,
+    mapelList: List<Mapel>,
+    emptyListText: String = "Anda tidak memiliki mata pelajaran apa pun.\n Klik tombol + untuk menambahkan mata pelajaran baru."
+) {
+    Column(modifier = modifier) {
+        Row (
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "Mata Pelajaran",
+                style = MaterialTheme.typography.bodySmall,
+                modifier = Modifier.padding(horizontal = 12.dp)
+            )
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Mapel"
+                )
+            }
+        }
+        if (mapelList.isEmpty()) {
+            Image(
+                modifier = Modifier
+                    .size(120.dp)
+                    .align(Alignment.CenterHorizontally),
+            painter = painterResource(id = R.drawable.img_books),
+            contentDescription = emptyListText
+            )
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = emptyListText,
+                style = MaterialTheme.typography.bodySmall,
+                color = Color.Gray,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
 }
