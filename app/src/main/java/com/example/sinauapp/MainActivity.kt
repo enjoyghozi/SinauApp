55538ef161b88745d5a6df0f37cccea2314dfe12
enjoyghozi <adamghosi03@gmail.com>
2023-12-20 15:33:47 +0700

    feat(ui) : HomeScreen

--- app/src/main/java/com/example/sinauapp/MainActivity.kt
+++ app/src/main/java/com/example/sinauapp/MainActivity.kt
@@ -11,36 +11,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.sinauapp.ui.theme.SinauAppTheme
+import com.example.sinauapp.ui.theme.home.HomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SinauAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                HomeScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SinauAppTheme {
-        Greeting("Android")
     }
 }
