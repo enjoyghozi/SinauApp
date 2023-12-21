04274818a987f05a9374d87ce7232634dc4132e5
enjoyghozi <adamghosi03@gmail.com>
2023-12-21 23:46:36 +0700

    fix(ui) : DatePicker on TaskScreen

--- app/src/main/java/com/example/sinauapp/ui/task/TaskScreen.kt
+++ app/src/main/java/com/example/sinauapp/ui/task/TaskScreen.kt
@@ -1,26 +1,92 @@
 package com.example.sinauapp.ui.task
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.example.sinauapp.ui.components.DeleteDialog
 import com.example.sinauapp.ui.components.TaskCheckbox
+import com.example.sinauapp.ui.components.TaskDatePicker
+import com.example.sinauapp.utility.Priority
+import com.example.sinauapp.utility.changeMillisToDateString
+import java.time.Instant
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TaskScreen() {
+    var isDeleteDialogOpen by remember { mutableStateOf(false) }
+
+    var isDatePickerDialogOpen by rememberSaveable { mutableStateOf(false) }
+    val datePickerState = rememberDatePickerState(
+        initialSelectedDateMillis = Instant.now().toEpochMilli()
+    )
+
+    var title by remember { mutableStateOf("") }
+    var description by remember { mutableStateOf("") }
+
+    var taskTitleError by remember { mutableStateOf<String?>(null) }
+    taskTitleError = when {
+        title.isBlank() -> "Silahkan isi judul task"
+        title.length < 3 -> "Judul task minimal 3 karakter"
+        title.length > 30 -> "Judul task terlalu panjang"
+        else -> null
+    }
+
+    DeleteDialog(
+        isOpen = isDeleteDialogOpen,
+        title = "Hapus Task?",
+        bodyText = "Apakah anda yakin ingin menghapus task ini?" + "\n" + "Task yang di hapus tidak dapat dikembalikan",
+        onDismissRequest = { isDeleteDialogOpen = false },
+        onConfirmButtonClick = {
+            isDeleteDialogOpen = false
+        }
+    )
+
+    TaskDatePicker(
+        state = datePickerState,
+        isOpen = isDatePickerDialogOpen,
+        onDismissRequest = { isDatePickerDialogOpen = false },
+        onConfirmButtonClicked = {
+            isDatePickerDialogOpen = false
+        }
+    )
+
     Scaffold (
         topBar =  {
             TaskScreenTopBar(
@@ -28,18 +94,113 @@ fun TaskScreen() {
                 isStatus = false,
                 checkBoxBorderColor = Color.Red,
                 onBackButtonClick = {},
-                onDeleteButtonClick = {},
+                onDeleteButtonClick = { isDeleteDialogOpen = true },
                 onCheckBoxClick = {}
             )
         }
-    ) { paddingValues ->
+    ) { paddingValue ->
         Column(
             modifier = Modifier
+                .verticalScroll(state = rememberScrollState())
                 .fillMaxSize()
-                .padding(paddingValues)
+                .padding(paddingValue)
                 .padding(horizontal = 12.dp)
         ) {
-
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = title,
+                onValueChange = { title = it },
+                label = {
+                    Text(text = "Judul")
+                },
+                singleLine = true,
+                isError = taskTitleError != null && title.isNotBlank(),
+                supportingText = {
+                    Text(text = taskTitleError.orEmpty())
+                }
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = description,
+                onValueChange = { description = it },
+                label = {
+                    Text(text = "Keterangan")
+                }
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = "Tanggal",
+                style = MaterialTheme.typography.bodySmall
+            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = datePickerState.selectedDateMillis.changeMillisToDateString(),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+                IconButton(onClick = { isDatePickerDialogOpen = true }) {
+                    Icon(
+                        imageVector = Icons.Default.DateRange,
+                        contentDescription = "Pilih Tanggal",
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(
+                text = "Prioritas",
+                style = MaterialTheme.typography.bodySmall
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Priority.entries.forEach { priority ->
+                    PriorityButton(
+                        modifier = Modifier.weight(1f),
+                        label = priority.title,
+                        backgroundColor = priority.color,
+                        borderColor = if (priority == Priority.Major) {
+                            Color.White
+                        } else Color.Transparent,
+                        labelColor = if (priority == Priority.Important) {
+                            Color.White
+                        } else Color.White.copy(alpha = 0.7f),
+                        onClick = {}
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(30.dp))
+            Text(
+                text = "Terkait dengan Mapel",
+                style = MaterialTheme.typography.bodySmall
+            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "English",
+                    style = MaterialTheme.typography.bodyLarge
+                )
+                IconButton(onClick = {  }) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowDropDown,
+                        contentDescription = "Select Subject"
+                    )
+                }
+            }
+            Button(
+                enabled = taskTitleError == null,
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 20.dp)
+            ) {
+                Text(text = "Save")
+            }
         }
     }
 }
@@ -66,6 +227,7 @@ private fun TaskScreenTopBar(
         title = {
             Text(
                 text = "Task",
+                style = MaterialTheme.typography.headlineSmall
             )
         },
         actions = {
@@ -85,4 +247,26 @@ private fun TaskScreenTopBar(
             }
         }
     )
+}
+
+@Composable
+private fun PriorityButton(
+    modifier: Modifier = Modifier,
+    label: String,
+    backgroundColor: Color,
+    borderColor: Color,
+    labelColor: Color,
+    onClick: () -> Unit
+) {
+    Box(
+        modifier = modifier
+            .background(backgroundColor)
+            .clickable { onClick() }
+            .padding(5.dp)
+            .border(1.dp, borderColor, RoundedCornerShape(5.dp))
+            .padding(5.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = label, color = labelColor)
+    }
 }
--- app/src/main/java/com/example/sinauapp/utility/Helper.kt
+++ app/src/main/java/com/example/sinauapp/utility/Helper.kt
@@ -4,6 +4,10 @@ import androidx.compose.ui.graphics.Color
 import com.example.sinauapp.ui.theme.green
 import com.example.sinauapp.ui.theme.orange
 import com.example.sinauapp.ui.theme.red
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 enum class Priority (
     val title: String,
@@ -17,4 +21,14 @@ enum class Priority (
     companion object {
         fun fromInt(value: Int) = values().firstOrNull { it.Value == value } ?: Major
     }
+}
+
+fun Long?.changeMillisToDateString(): String {
+    val date: LocalDate = this?.let {
+        Instant
+            .ofEpochMilli(it)
+            .atZone(ZoneId.systemDefault())
+            .toLocalDate()
+    } ?: LocalDate.now()
+    return date.format(DateTimeFormatter.ofPattern("dd MMM yyyy"))
 }
